9290ecda8ac0e44d2bfbaaf1a93ceb74c4295431
damianwebelevate <damian.orourke@webelevate.ie>
2015-01-21 22:54:06 +0000

    finalised css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 
+        //   var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
+
+          
+
+        //   var y = parseInt(w.x);
+
+        //   function workOut(){
+        //   var out = document.getElementById("show_hide");
+        //   var dosomething = document.createElement("img");
+        //   var addText = document.createTextNode("something to say");
+        //   var hone = dosomething.appendChild(addText);
+
+        //   if(y <= 600){
+        //     out.appendChild(hone);
+        //     console.log(y);
+        //   }
+        //   else{
+        //     out.style.display = "none";
+        //   }
+        // }
+
